6905e98d9134cd77ae38269e1ffa05edc309f30c
박철한 <parkcheolhan@bluedigm.com>
2019-03-21 10:40:23 +0900

    branch 테스트

--- src/main/java/org/openpaas/paasta/portal/storage/api/StorageApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/storage/api/StorageApiApplication.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * PaaS-TA Storage API application. 
  * (org.openpaas.paasta.portal.storage.api)
- * 
- * @author hgcho (Hyungu Cho)
+ *
  * @version 
  * @since 2018. 4. 2.
  *
