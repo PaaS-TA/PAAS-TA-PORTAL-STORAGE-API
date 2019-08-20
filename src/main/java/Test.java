a9769cba49575a01c4e81e70549cef272c3d2f2e
swmoon <swmoon@bluedigm.com>
2019-08-20 10:57:56 +0900

    ObjectStorage 업그레이도 인한, 소스 수정

---
+++ src/main/java/Test.java
@@ -0,0 +1,20 @@
+import org.javaswift.joss.client.factory.AccountConfig;
+import org.javaswift.joss.client.factory.AccountFactory;
+import org.javaswift.joss.client.factory.AuthenticationMethod;
+import org.javaswift.joss.model.Account;
+
+public class Test {
+
+    public static void main(String[] args) {
+        AccountConfig config = new AccountConfig();
+//        config.setTenantName("service");
+        config.setUsername("paasta-portal");
+        config.setPassword("paasta");
+        config.setAuthUrl("http://115.68.47.168:5000/v2.0/tokens");
+        config.setAuthenticationMethod(AuthenticationMethod.KEYSTONE);
+//        config.setDomain("default");
+        Account account = new AccountFactory(config).createAccount();
+
+
+    }
+}
